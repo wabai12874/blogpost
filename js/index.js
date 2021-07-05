10dcf8697dccbafd23bb174f87ee75bf1c2a3ea7
wabai12874 <brianmaringa19@gmail.com>
2021-07-05 16:45:35 +0300

    added zoom

--- js/index.js
+++ js/index.js
@@ -7,3 +7,24 @@ function showIcons1(x) {
 function showIcons2(x) {
     document.getElementsByClassName('sharebtns')[2].style.display = 'initial';
 }
+
+function openside1() {
+    document.getElementById("main").style.marginRight = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+    document.getElementById("closeNav").style.display = 'initial';
+
+}
+    function closeside1() {
+    document.getElementById("main").style.marginRight = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "initial";
+    document.getElementById("closeNav").style.display = 'none';
+
+}
+
+function showText(){
+    document.getElementById("content2").style.display = "initial";
+}
+
