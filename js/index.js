d155d4fc888e9922984b2c6c389f3d1656237ad9
wabai12874 <brianmaringa19@gmail.com>
2021-07-05 14:14:57 +0300

    yes

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showIcons(x) {
+    document.getElementsByClassName('sharebtns')[0].style.display = 'initial';
+}
+function showIcons1(x) {
+    document.getElementsByClassName('sharebtns')[1].style.display = 'initial';
+}
+function showIcons2(x) {
+    document.getElementsByClassName('sharebtns')[2].style.display = 'initial';
+}
